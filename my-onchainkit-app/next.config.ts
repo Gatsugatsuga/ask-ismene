50362cc2c1049ff6192444aac069a9e7654ce6cd
Gatsugatsuga <f.ckartletsdance@gmail.com>
2025-12-27 17:47:33 +0900

    Add empty turbopack config to silence warning

--- my-onchainkit-app/next.config.ts
+++ my-onchainkit-app/next.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname, '../'),
+  turbopack: {},
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
