ccf2f2dd24244b3293aefdede87b7287db64cd0f
Gatsugatsuga <f.ckartletsdance@gmail.com>
2025-12-28 16:36:01 +0900

    Restore original working next.config.ts

--- my-onchainkit-app/next.config.ts
+++ my-onchainkit-app/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, '../'),
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
 
 export default nextConfig;
