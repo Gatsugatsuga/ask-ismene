5b0aef264acec1a6e49b3b746af6e53380a457d4
Gatsugatsuga <f.ckartletsdance@gmail.com>
2025-12-28 15:40:54 +0900

    Remove webpack config to fix SDK loading

--- my-onchainkit-app/next.config.ts
+++ my-onchainkit-app/next.config.ts
@@ -3,11 +3,6 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname, '../'),
-  turbopack: {},
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
 };
 
 export default nextConfig;
