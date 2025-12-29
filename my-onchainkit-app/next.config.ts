9ee114cadac180df88542b3b06e344960c5e0b38
Gatsugatsuga <f.ckartletsdance@gmail.com>
2025-12-29 10:10:51 +0900

    Remove all webpack externals to test SDK

--- my-onchainkit-app/next.config.ts
+++ my-onchainkit-app/next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {},
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs");
-    return config;
-  },
 };
 
 export default nextConfig;
